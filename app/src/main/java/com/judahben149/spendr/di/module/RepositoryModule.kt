48b60a6e9ac0e77698e6a2eb85665d7675d99892
Judah <judahben149@gmail.com>
2023-05-23 04:00:13 +0100

    DI module for database repository

--- app/src/main/java/com/judahben149/spendr/di/module/RepositoryModule.kt
+++ app/src/main/java/com/judahben149/spendr/di/module/RepositoryModule.kt
@@ -2,6 +2,7 @@ package com.judahben149.spendr.di.module
 
 import com.judahben149.spendr.data.local.CashFlowDao
 import com.judahben149.spendr.data.repository.CashFlowRepositoryImpl
+import com.judahben149.spendr.data.repository.DatabaseRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,5 +17,11 @@ object RepositoryModule {
     @Singleton
     fun providesCashFlowRepository(cashFlowDao: CashFlowDao): CashFlowRepositoryImpl {
         return CashFlowRepositoryImpl(cashFlowDao)
+    }
+
+    @Provides
+    @Singleton
+    fun providesDatabaseRepository(cashFlowDao: CashFlowDao): DatabaseRepositoryImpl {
+        return DatabaseRepositoryImpl(cashFlowDao)
     }
 }
