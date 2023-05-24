09d9381c8b8f4e3df14e0ca51b121ff5a738f8ec
Judah <judahben149@gmail.com>
2023-05-24 12:54:29 +0100

    Created entry list and entry detail fragments and added to nav graph

--- app/src/main/java/com/judahben149/spendr/utils/Constants.kt
+++ app/src/main/java/com/judahben149/spendr/utils/Constants.kt
@@ -8,4 +8,8 @@ object Constants {
     const val DATE_PICKER_ADD_CASH_ENTRY = "DATE_PICKER_ADD_CASH_ENTRY"
     const val SETTINGS_FILE = "app_pref"
     const val IS_FIRST_LAUNCH = "is_first_launch"
+
+    //Keys
+    const val ENTRY_TYPE = "ENTRY_TYPE"
+    const val ENTRY_ID = "ENTRY_ID"
 }
