8ae5b2c871c5aa19b34c2b211cd1db4a0fefffbc
Judah <judahben149@gmail.com>
2023-05-24 11:46:31 +0100

    Set up cash summary epoxy controller state and added progress bar

---
+++ app/src/main/java/com/judahben149/spendr/presentation/cashflow_summary/epoxy/model/SummaryProgressBarEpoxyModel.kt
@@ -0,0 +1,15 @@
+package com.judahben149.spendr.presentation.cashflow_summary.epoxy.model
+
+import android.view.View
+import com.judahben149.spendr.R
+import com.judahben149.spendr.databinding.EpoxyModelProgressBarBinding
+import com.judahben149.spendr.presentation.cashflow_summary.epoxy.ViewBindingKotlinModel
+
+data class SummaryProgressBarEpoxyModel(
+    val nothing: String? = null
+): ViewBindingKotlinModel<EpoxyModelProgressBarBinding>(R.layout.epoxy_model_progress_bar) {
+
+    override fun EpoxyModelProgressBarBinding.bind() {
+        pgBarSummary.visibility = View.VISIBLE
+    }
+}
