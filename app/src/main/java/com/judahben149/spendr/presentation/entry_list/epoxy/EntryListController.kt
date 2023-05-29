450c9e0eeb02359e8e39b99e5a1826a866feb38e
Judah <judahben149@gmail.com>
2023-05-29 21:01:39 +0100

    Added month header model section to controller

--- app/src/main/java/com/judahben149/spendr/presentation/entry_list/epoxy/EntryListController.kt
+++ app/src/main/java/com/judahben149/spendr/presentation/entry_list/epoxy/EntryListController.kt
@@ -1,25 +1,49 @@
 package com.judahben149.spendr.presentation.entry_list.epoxy
 
-import android.util.Log
 import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.paging3.PagingDataEpoxyController
-import com.judahben149.spendr.domain.model.CashEntry
+import com.judahben149.spendr.domain.model.EntryListData
 import com.judahben149.spendr.presentation.cashflow_summary.epoxy.model.SummaryEntryItemEpoxyModel
+import com.judahben149.spendr.presentation.entry_list.epoxy.model.EntryListMonthHeaderEpoxyModel
+import com.judahben149.spendr.utils.Constants.TIMBER_TAG
 import com.judahben149.spendr.utils.DateUtils
 import com.judahben149.spendr.utils.extensions.abbreviateNumber
+import timber.log.Timber
 
 class EntryListController(
-    private val onEntryItemClicked:(id: Int) -> Unit
-): PagingDataEpoxyController<CashEntry>() {
+    private val onEntryItemClicked: (id: Int) -> Unit
+) : PagingDataEpoxyController<EntryListData>() {
 
-    override fun buildItemModel(currentPosition: Int, entry: CashEntry?): EpoxyModel<*> {
+    override fun buildItemModel(currentPosition: Int, item: EntryListData?): EpoxyModel<*> {
 
-        Log.d("TAGM", "getALlPagedCashEntries: controller")
-        return SummaryEntryItemEpoxyModel(
-            amount = entry?.amount?.abbreviateNumber()!!,
-            date = DateUtils.formatFriendlyDateTime(entry?.transactionDate!!),
-            category = "Savings",
-            onEntryItemClicked = { onEntryItemClicked(entry?.id!!) }
-        ).id("pagedCashEntry")
+        return when (item) {
+            is EntryListData.EntryItem -> {
+                SummaryEntryItemEpoxyModel(
+                    amount = item.entryItem.amount.abbreviateNumber(),
+                    date = DateUtils.formatFriendlyDateTime(item.entryItem.transactionDate),
+                    category = "Savings",
+                    onEntryItemClicked = { onEntryItemClicked(item.entryItem.id) }
+                ).id("pagedCashEntry")
+            }
+
+            is EntryListData.MonthHeader -> {
+                EntryListMonthHeaderEpoxyModel(
+                    monthName = item.month
+                ).id("monthHeader")
+            }
+
+            else -> error("unsupported model type")
+        }
+    }
+
+    override fun isStickyHeader(position: Int): Boolean {
+        val item = adapter.getModelAtPosition(position)
+        Timber.tag(TIMBER_TAG).d("is sticky header called")
+
+        if (item is EntryListMonthHeaderEpoxyModel) {
+            Timber.tag(TIMBER_TAG).d("is sticky header called in IF statement")
+            return true
+        }
+        return false
     }
 }
