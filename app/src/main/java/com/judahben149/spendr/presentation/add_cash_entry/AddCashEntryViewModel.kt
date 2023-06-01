3ddc079b7c228135610455c3106ecad687327673
Judah <judahben149@gmail.com>
2023-06-01 10:15:27 +0100

    Completed category bottom sheet functionality

--- app/src/main/java/com/judahben149/spendr/presentation/add_cash_entry/AddCashEntryViewModel.kt
+++ app/src/main/java/com/judahben149/spendr/presentation/add_cash_entry/AddCashEntryViewModel.kt
@@ -3,6 +3,7 @@ package com.judahben149.spendr.presentation.add_cash_entry
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.judahben149.spendr.data.repository.CashFlowRepositoryImpl
 import com.judahben149.spendr.domain.mappers.MapperImpl
@@ -10,6 +11,8 @@ import com.judahben149.spendr.domain.model.CashEntry
 import com.judahben149.spendr.domain.model.Category
 import com.judahben149.spendr.utils.Constants.TIMBER_TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -21,12 +24,24 @@ class AddCashEntryViewModel @Inject constructor(private val repository: CashFlow
     private var _state: MutableLiveData<AddCashEntryState> = MutableLiveData(AddCashEntryState())
     val state: LiveData<AddCashEntryState> = _state
 
-    private var _categoryState: MutableLiveData<CategoryListState> = MutableLiveData(CategoryListState())
+    private var _categoryState: MutableLiveData<CategoryListState> =
+        MutableLiveData(CategoryListState())
     val categoryState: LiveData<CategoryListState> = _categoryState
 
     private var _selectedCategoryId: MutableLiveData<Int> = MutableLiveData(-1)
     val selectedCategoryId: LiveData<Int> get() = _selectedCategoryId
 
+    private var _selectedCategoryList: MutableLiveData<List<Category>> =
+        MutableLiveData(emptyList())
+    val selectedCategoryList: LiveData<List<Category>> get() = _selectedCategoryList
+
+    private var _selectedCategory: MutableLiveData<Category> = MutableLiveData(Category())
+    val selectedCategory: LiveData<Category> get() = _selectedCategory
+
+
+    init {
+        getCategoryList()
+    }
 
     fun updateAmount(amount: Int) {
         _state.value = _state.value?.copy(
@@ -39,7 +54,8 @@ class AddCashEntryViewModel @Inject constructor(private val repository: CashFlow
             val cashEntry = CashEntry(
                 amount = _state.value!!.amount.toDouble(),
                 isIncome = _state.value!!.isIncome,
-                transactionDate =_state.value!!.date
+                transactionDate = _state.value!!.date,
+                categoryId = _selectedCategory.value!!.categoryId
             )
 
             val entity = MapperImpl().cashEntryToCashEntryEntity(cashEntry)
@@ -47,9 +63,24 @@ class AddCashEntryViewModel @Inject constructor(private val repository: CashFlow
         }
     }
 
+    fun getCategoryList() {
+        viewModelScope.launch {
+            repository.getCategories().collect { categoryEntityList ->
+                val categoryList = categoryEntityList.map { categoryEntity ->
+                    MapperImpl().categoryEntityToCategory(categoryEntity)
+                }
+
+                _selectedCategoryList.value = categoryList
+            }
+        }
+    }
+
 
     fun updateSelectedCategoryId(categoryId: Int) {
         _selectedCategoryId.value = categoryId
+        _selectedCategory.value = _selectedCategoryList.value?.find { category ->
+            category.categoryId == categoryId
+        }
     }
 
     fun setCurrentDate(currentDate: Long) {
