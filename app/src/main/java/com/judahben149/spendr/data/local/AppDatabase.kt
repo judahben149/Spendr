35ee7f441331d1a76e5b9ea648dc876b6f5544b3
Judah <judahben149@gmail.com>
2023-06-02 21:26:46 +0100

    Added a feature to create payment reminders

--- app/src/main/java/com/judahben149/spendr/data/local/AppDatabase.kt
+++ app/src/main/java/com/judahben149/spendr/data/local/AppDatabase.kt
@@ -4,9 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.judahben149.spendr.data.local.entity.CashEntryEntity
 import com.judahben149.spendr.data.local.entity.CategoryEntity
+import com.judahben149.spendr.data.local.entity.ReminderEntity
 
-@Database(entities = [CashEntryEntity::class, CategoryEntity::class], exportSchema = false, version = 1)
+@Database(entities = [CashEntryEntity::class, CategoryEntity::class, ReminderEntity::class], exportSchema = false, version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun CashFlowDao(): CashFlowDao
+
+    abstract fun RemindersDao(): RemindersDao
 }
