63a4b9e3f9565ca12ae0150475966519ffa3bcc9
Judah <judahben149@gmail.com>
2023-06-07 03:08:26 +0100

    Updated app constants

--- app/src/main/java/com/judahben149/spendr/utils/Constants.kt
+++ app/src/main/java/com/judahben149/spendr/utils/Constants.kt
@@ -5,6 +5,7 @@ object Constants {
     const val CATEGORY_BOTTOM_SHEET = "CATEGORY_BOTTOM_SHEET"
     const val REMINDER_BOTTOM_SHEET = "REMINDER_BOTTOM_SHEET"
     const val APP_DATABASE_NAME = "spendr_database"
+    const val USER_NAME = "USER_NAME"
 
     //Tags
     const val DATE_PICKER_ADD_CASH_ENTRY = "DATE_PICKER_ADD_CASH_ENTRY"
@@ -21,6 +22,8 @@ object Constants {
     const val REMINDER_TITLE = "REMINDER_TITLE"
     const val REMINDER_TEXT = "REMINDER_TEXT"
     const val NOTIFICATION_BUNDLE = "NOTIFICATION_BUNDLE"
+
+    const val ONBOARDING_FRAGMENT_PAGE = "ONBOARDING_FRAGMENT_PAGE"
 
     //Constants
     const val INITIAL_PAGE = 0
