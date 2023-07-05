991d65fea0a54ea1bb4abd9bdfa65ad8c899df6a
Judah <judahben149@gmail.com>
2023-07-05 15:18:08 +0100

    Wrote tests for RemindersDao database operations

--- app/src/main/java/com/judahben149/spendr/data/local/RemindersDao.kt
+++ app/src/main/java/com/judahben149/spendr/data/local/RemindersDao.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface RemindersDao {
 
-    @Upsert()
+    @Upsert
     suspend fun saveReminder(reminderEntity: ReminderEntity)
 
     @Query("SELECT * FROM reminders ORDER BY targetDate ASC")
