4c3b3ab7c76e40f870079827a28414634137fa4c
Judah <judahben149@gmail.com>
2024-02-25 02:17:02 +0100

    Created function to show toast

---
+++ app/src/main/java/com/judahben149/spendr/utils/ViewUtils.kt
@@ -0,0 +1,8 @@
+package com.judahben149.spendr.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun showToast(context: Context?, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+}
